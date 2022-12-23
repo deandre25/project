50ee6b0b835b7fadf887745f9a0502e611b559e0
Andrew <deyneka2003@gmail.com>
2022-12-23 23:29:43 +0200

    second commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 "use strict";
 
-const category='toys';
+console.log(4+ '-5');
 
-console.log(`https://someurl.com/${category}/5`);
+let incr = 10,
+    decr = 10;
 
-const user = 'andrew';
+// ++incr;
+// --decr;
 
-alert(`hello, ${user}`);
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2+2*2 !== '6');
+
+const isChecked = false,
+    isClose = true;
+
+console.log(isChecked || !isClose);
